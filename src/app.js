f601a02e61e082db4ec78e451fa432f1dc6dd420
Franco Castany <francocastany@gmail.com>
2023-03-19 16:05:49 -0300

    Message changed

--- src/app.js
+++ src/app.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening in ${PORT}`));
 
 app.get('/', (req, res) => {
-    res.send('Hola Railway =D')
+    res.send('Hola Railway App... =)')
 });
